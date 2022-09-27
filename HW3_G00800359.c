a8a92f63467d1d03d581eadd2281cab7aed85c79
Alessandro Gaiarin <agaiarin@gmail.com>
2022-09-27 13:54:57 -0400

    completed getInteger()

--- HW3_G00800359.c
+++ HW3_G00800359.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 char *getDateAndTime(); 
 // returns char string with current date and time
 int getInteger(); 
@@ -13,10 +15,64 @@ void decimalToOct(int decValue, char octString[]);
 // Store resulting octal value as char string within octString.
 
 int main() {
+    int userNum = 0;
+    char userName[36]; // i really have no clue what to initialize these to
+    printf("Enter your name: ");
+    fgets(userName, sizeof(userName, stdin)); //get user's name
+
+    userNum = getInteger();
+    while (userNum != -1) { //exit case
+        
+    }
+
+}
+
+char *getDateAndTime() {
+    return 0;
+}
+
+int getInteger() {
+    char buffer[256]; //set buffer to 7, since there can only be a max of 7 ints?
+    int convertedInt = 0;
+    printf("Enter an Integer [1 - 1000000] or type x to exit: ");
+    fgets(buffer, 256, stdin); //wonder if this works
+    if (buffer[0] == 'x') {
+        return -1;
+    }
+     convertedInt = atoi(buffer); //wonder if THIS works
+     if (convertedInt >= 1 && convertedInt <= 1000000) {
+        return convertedInt;
+     }
+     else {
+        printf("Error: %d is out of range", convertedInt);
+        return getInteger(); //recursion!
+     }
+
+}
+
+void decimalToBinary(int decValue, char binString[]) {
+    int quotient = 0;
+
+}
+
+void decimalToHex(int decValue, char hexString[]) {
+
+}
+
+void decimalToOct(int decValue, char octString[]) {
+
+}
+
+void saveUserFile() {
+
+}
+
 /*
+
+int main() {
     userNum = 0;
     userName = printf("Enter your name: ");
-    
+
     userNum = printf("Enter an Integer [1 - 1000000] or type x to exit: ");
     while (userNum != "x") {
         while (!(1 < userNum < 1000000)) {
@@ -27,7 +83,7 @@ int main() {
                 exit()
             }
         }
-    
+
         printf(\n);
         printf("Decimal: " + userNum);
         printf("Hexadecimal: " + decimalToHex(userNum));
@@ -35,25 +91,28 @@ int main() {
         printf("Binary: " + decimalToBinary(userNum));
 
         //prompt for file - THIS SHOULD BE ITS OWN METHOD
-        void saveFile() {
-            userFile = printf("Save to a file? (y/n): ");
-            if (userFile == "n") {
-                continue;
-            }
-            else if (userFile == "y") {
-                fileName = printf("Enter file name: ");
-                createFile(fileName) //this is a fake method
-                printf("File saved.");
-            }
-            else {
-                printf("Error: illegal value");
-                saveFile();
-            }
-        }
+        saveFile();
 
+        //Set loop up to be restarted:
+        userNum = printf("Enter an Integer [1 - 1000000] or type x to exit: ");
     }
     return 0;
+}
 
+void saveFile() {
+    userFile = printf("Save to a file? (y/n): ");
+    if (userFile == "n") {
+        continue;
+    }
+    else if (userFile == "y") {
+        fileName = printf("Enter file name: ");
+        createFile(fileName) //this is a fake method
+        printf("File saved.");
+    }
+    else {
+        printf("Error: illegal value");
+        saveFile();
+    }
+}
 
 */
-}
