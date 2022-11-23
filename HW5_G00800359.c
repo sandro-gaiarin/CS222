3527313cd630a739cabe4eff268c95bf1da09b8e
Alessandro Gaiarin <agaiarin@gmail.com>
2022-11-22 19:27:17 -0500

    implemented initial readDataFile()

--- HW5_G00800359.c
+++ HW5_G00800359.c
@@ -3,15 +3,10 @@
 #include <string.h>
 #include <time.h>
 
-struct address {
-    char mac1[2];
-    char mac2[2];
-    char mac3[2];
-    char mac4[2];
-    char mac5[2];
-    char mac6[2];
-    char macAlias[16];
-} address_t;
+struct address_t {
+    int macArray[6]; //array to store mac address
+    char macAlias[16]; //character array to store the mac alias 
+} address;
 /*
 Open and read data file (CS222_Inet.txt)
 While reading, generate the CS222_Error_Report.txt file
@@ -19,19 +14,20 @@ Dynamically allocate array of address_t structs
 rewind() CS222_Inet.txt file
 Populate dynamically allocated array
 */
-void readDataFile(char fileName[]);
+void readDataFile();
 /*
 Confirm the MAC address components are in range 00 to FF
 */
-int checkAddress(address_t macAddress);
+int checkAddress(address macAddress);
 /*
 Confirm that alias has strlen() <= 16
 */
-int checkAlias(address_t macAddress);
+int checkAlias(address macAddress);
 int generateManufacturerRpt(); //TODO check return val on this
 char *getDateAndTime(); //reusing old code for this one
 
 int main() {
+    readDataFile();
     //TODO
 }
 
@@ -39,4 +35,23 @@ char *getDateAndTime() {
     time_t t;
     time(&t);
     return ctime(&t);
+}
+
+void readDataFile() {
+    char fileName[] = "CS222_Inet.txt";
+    FILE *filePointer;
+    filePointer = fopen(fileName, "r"); //point filePointer at file
+
+    char charBuffer[50];
+    int maxLineLength = 60;
+
+    if (filePointer == NULL) { //error for if the file cannot be opened
+        printf("Unable to open 'CS222_Inet.txt' file.\n");
+        exit(1);
+    }
+    
+    while (fgets(charBuffer, maxLineLength, filePointer)) {
+        printf(charBuffer);
+    }
+
 }
