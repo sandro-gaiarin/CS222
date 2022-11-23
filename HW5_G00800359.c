f497f68bef17cb98853ed3370b80278951feda45
Alessandro Gaiarin <agaiarin@gmail.com>
2022-11-23 14:33:16 -0500

    bug

--- HW5_G00800359.c
+++ HW5_G00800359.c
@@ -94,7 +94,7 @@ void readDataFile() {
             break; // break out of while loop if we reach the NONE alias
         }
         //addressArrayPointer_g[i] = buildAddressStruct(charBuffer); // add the build address_t structs to the malloc'd array
-        addressArray[i] = *buildAddressStruct(charBuffer); // add the build address_t structs to the malloc'd array
+        addressArray[i] = &buildAddressStruct(charBuffer); // add the build address_t structs to the malloc'd array
         i++;
     }
     fclose(filePointer); // close the file
