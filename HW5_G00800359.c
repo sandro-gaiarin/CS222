da175a1cc019b32b6ac69860d86de8d8d24dbeb1
Alessandro Gaiarin <agaiarin@gmail.com>
2022-11-26 18:25:19 -0500

    attempting to fix fopen bug

--- HW5_G00800359.c
+++ HW5_G00800359.c
@@ -107,6 +107,7 @@ void readDataFile() {
 
     FILE *errorFile; // create error file
     errorFile = fopen("222_Error_Report.txt", "w");
+    printf("Error file open attempt created");
     if (errorFile == NULL) { //TEST
         perror("The following error occurred.");
     }
